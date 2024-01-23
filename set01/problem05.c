61b8e4ec674dd193cdf6f2fb31385de52a8a51be
Pooja Goudar <poojag.bsc23@rvu.edu.in>
2024-01-24 00:36:26 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,4 +1,4 @@
-5. Write a C program to compare three numbers using **pass by value**
+/*5. Write a C program to compare three numbers using **pass by value**
 
 ***Function Declarations***
 
@@ -23,6 +23,45 @@ The largest of 1,2 and 3 is 3.
 ```
 
 
----
+---*/
+
+#include <stdio.h>
+
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+
+int main() {
+    int a,b,c,largest;
+    a = input();
+    b = input();
+    c = input();
+    largest = compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+}
+
+int input() {
+    int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    return x;
+}
+
+int compare(int a, int b, int c){
+    if (a >= b && a >= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+        return c;
+}
+
+void output(int a, int b, int c, int largest) {
+    printf("The largest of %d, %d, and %d is %d.\n", a, b, c, largest);
+}
+
+
+
 
 
--- set01/problem06.c
+++ set01/problem06.c
@@ -1,4 +1,4 @@
-6. Write a C program to compare three numbers using **pass by reference**
+/*6. Write a C program to compare three numbers using **pass by reference**
 
 ***Function Declarations***
 
@@ -22,4 +22,44 @@ void output(int a, int b, int c, int largest);
 1
 ```
 
----
+---*/
+
+#include <stdio.h>
+
+// Function declarations
+int input(int *a, int *b, int *c);
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
+
+int main() {
+    int a,b,c,largest;
+    input(&a, &b, &c);
+    compare(a,b,c, &largest);
+    output(a,b,c, largest);
+    return 0;
+}
+
+int input(int *a, int *b, int *c) {
+    printf("Enter the first number: ");
+    scanf("%d", a);
+    printf("Enter the second number: ");
+    scanf("%d", b);
+    printf("Enter the third number: ");
+    scanf("%d", c);
+    return 0;
+}
+
+void compare(int a, int b, int c, int *largest) {
+    if (a >= b && a >= c)
+        *largest = a;
+    else if (b >= a && b >= c)
+        *largest = b;
+    else
+        *largest = c;
+}
+
+void output(int a, int b, int c, int largest) {
+    printf("%d\n", largest);
+}
+
+
